4300dd4aeca0ee210ef3b593ea6b3f677c9bc747
SnekNOTSnake <udintobe@gmail.com>
2020-08-13 22:12:59 +0700

    Add airbnb code stylings

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
-// Enable Webpack HMR. Don't worry about production
-// It only leaves very small footprint
-if (module.hot) module.hot.accept();
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import './styles/index.css';
+
+// Enable Webpack HMR. Don't worry about production
+// It only leaves very small footprint
+if (module.hot) module.hot.accept();
 
 ReactDOM.render(
 	<React.StrictMode>
